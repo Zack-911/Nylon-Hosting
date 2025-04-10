ef91adbc3782308098f108b46bb2a49b2bc9866f
Zack <muhammadbinasim471@gmail.com>
2025-04-10 13:07:26 +0000

    clean repo

--- README.md
+++
@@ -1,3 +0,0 @@
-# WIP
-This project is a WIP.
-It is not recommended to be used
--- components/dashboard/Separator.tsx
+++
@@ -1,6 +0,0 @@
-import { Separator as UISeparator } from "@/components/ui/separator"
-
-export default function Separator() {
-  return <UISeparator className="my-2 bg-slate-700" />
-}
-
